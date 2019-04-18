e07d521461854ea1350abe195261a6f09127e267
Salv95-2 <salvramirez95@gmail.com>
2019-04-17 17:10:39 -0700

    Adding routing nav menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent} from './contact/contact.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'AboutMe',
+    component: AboutMeComponent
+  },
+  {
+    path:'Contact',
+    component: ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
